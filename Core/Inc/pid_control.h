06ac6d361cc4e74137af88539d629af123d8f950
Salavat Magazov <s.magazov@erbamannheim.com>
2022-01-26 17:58:18 +0000

    feature: first draft rewrite of the PID controller.

--- Core/Inc/pid_control.h
+++ Core/Inc/pid_control.h
@@ -7,33 +7,67 @@
  *      Author: niuslar
  */
 
-#ifndef INC_HEATERS_H_
-#define INC_HEATERS_H_
+// these include guards should have the name of the file they are guarding.
+#ifndef PID_CONTROL_H_
+#define PID_CONTROL_H_
 
-#include "main.h"
-
-#define DEFAULT_TARGET  200
-#define DEFAULT_KP		1
-#define DEFAULT_KI		0
-#define DEFAULT_KD		0
+// uncomment this line if you want PID loop to perform limit checking of variables.
+#define LIMIT_CHECKING
 
 /**
  * @brief PID Handler
+ *
+ * This structure contains all information for a specific PID loop to function.
+ * For each instance of the loop, declare a single structure and then call
+ * initialisation function for it.
  */
 typedef struct{
 	float kp;
 	float ki;
 	float kd;
-	float set_point;
-	float prev_output;
-	float prev_error;
-	float cum_error;
-	uint32_t previous_time;
+#ifdef LIMIT_CHECKING
+	float max_p;
+	float min_p;
+	float max_i;
+	float min_i;
+	float max_d;
+	float min_d;
+	float max_out;
+	float min_out;
+#endif
+	float old_output;
+	float old_error;
+	float old_integral;
 }pid_handle_t;
 
 /* Exported Functions Prototypes */
-void PIDInit(pid_handle_t* pid_handler);
-float PIDControlOutput(pid_handle_t* pid_handler, float input);
-void setPIDTarget(pid_handle_t* pid_handler, float target);
 
-#endif /* INC_HEATERS_H_ */
+/**
+ * @brief Initialise newly created PID control structure.
+ *
+ * This will also reset all parameters to default values except control
+ * coefficients.
+ *
+ * @param p_pid_handler Pointer to the PID handler data structure.
+ */
+void initPID(pid_handle_t* p_pid_handler);
+
+/**
+ * @brief Reset PID control to freshly initialised state.
+ *
+ * @param p_pid_handler Pointer to the PID handler data structure.
+ */
+void resetPID(pid_handle_t* p_pid_handler);
+
+/**
+ * @brief Run PID loop once and calculate new output.
+ *
+ * @param p_pid_handler Pointer to the PID handler data structure.
+ * @param target_value Target value for the control variable.
+ * @param actual_value Actual value of the control variable.
+ *
+ * @return New output of the PID loop.
+ */
+float runPID(pid_handle_t* p_pid_handler, float target_value, float actual_value);
+
+#endif /* PID_CONTROL_H_ */
