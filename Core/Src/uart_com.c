3e370e686add9583669d6af4f85f16375bea5546
Niuslar <niuslar@outlook.com>
2022-01-26 13:04:13 +0000

    Change UART to send string instead of binary
    - In main.c, use huart2 instead of huart1 to work with ST-LINK uart
    - In .cproject, change setting to use float with sprintf
    - Change uart function to send string buffer.

--- Core/Src/uart_com.c
+++ Core/Src/uart_com.c
@@ -11,10 +11,12 @@
 #include "main.h"
 #include "uart_com.h"
 #include "adc_temp.h"
+#include "stdio.h"
 
 /* Private variables */
 static uint32_t time_stamp = 0;
 static uint32_t send_delay = 50; /* Min time delay between calls in ms*/
+static char uart_tx_buf[ADC_CHANNELS][TX_MSG_LEN];
 
 /**
   * @brief Sends selected data via blocking UART
@@ -34,8 +36,12 @@ void uartSendData(UART_HandleTypeDef *huart, uint8_t adc_or_celsius)
 		if(adc_or_celsius == SEND_ADC)
 		{
 			const uint16_t* tmp_p = readADCData();
-			/* ADC_CHANNELS * 2 is the size of the transfer */
-			if(HAL_TIMEOUT == HAL_UART_Transmit(huart, (uint8_t*)tmp_p, ADC_CHANNELS*2, UART_TIMEOUT))
+			for(uint8_t channel = 0; channel < ADC_CHANNELS; channel++)
+			{
+				sprintf(uart_tx_buf[channel], "ADC CH.%d is: %d\n\r", channel+1, tmp_p[channel]);
+			}
+
+			if(HAL_TIMEOUT == HAL_UART_Transmit(huart, (uint8_t*)uart_tx_buf, ADC_CHANNELS*TX_MSG_LEN, UART_TIMEOUT))
 			{
 				Error_Handler();
 			}
@@ -43,8 +49,12 @@ void uartSendData(UART_HandleTypeDef *huart, uint8_t adc_or_celsius)
 		else if(adc_or_celsius == SEND_CELSIUS)
 		{
 			const float* tmp_p = readTempSensors();
-			/* Because "float" is 4 bytes, size of transfer is ADC_CHANNELS *4 */
-			if(HAL_OK != HAL_UART_Transmit(huart, (uint8_t*)tmp_p, ADC_CHANNELS*4, UART_TIMEOUT))
+			for(uint8_t channel = 0; channel < ADC_CHANNELS; channel++)
+			{
+				sprintf(uart_tx_buf[channel], "Temp CH.%d is: %.2f\n\r", channel+1, tmp_p[channel]);
+			}
+
+			if(HAL_OK != HAL_UART_Transmit(huart, (uint8_t*)uart_tx_buf, ADC_CHANNELS*TX_MSG_LEN, UART_TIMEOUT))
 			{
 				Error_Handler();
 			}
