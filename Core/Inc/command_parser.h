2bff4f7171a6e1585fe6369ff9426f49e402d2d2
Salavat Magazov <s.magazov@erbamannheim.com>
2022-01-27 18:02:50 +0000

    wip: setup basic structure of the command parser... tea time.

---
+++ Core/Inc/command_parser.h
@@ -0,0 +1,33 @@
+/*
+ * command_parser.h
+ *
+ *  Created on: Jan 27, 2022
+ *      Author: salavat.magazov
+ */
+
+#ifndef INC_COMMAND_PARSER_H_
+#define INC_COMMAND_PARSER_H_
+
+#include <stdint.h>
+
+#define MAX_COMMAND_SIZE 50
+#define MAX_CHECKSUM_SIZE 2
+
+enum parser_state_t
+{
+	ESTATE_IDLE = 0,
+	ESTATE_COMMAND,
+	ESTATE_CHECKSUM
+};
+
+typedef struct COMMAND_PARSER_T
+{
+	char command[MAX_COMMAND_SIZE];
+	char checksum[MAX_CHECKSUM_SIZE];
+	char* p_progress;
+	enum parser_state_t parser_state;
+}command_parser_t;
+
+void initParser(command_parser_t *p_parser);
+
+#endif /* INC_COMMAND_PARSER_H_ */
---
+++ Core/Src/command_parser.c
@@ -0,0 +1,93 @@
+/*
+ * command_parser.c
+ *
+ *  Created on: Jan 27, 2022
+ *      Author: salavat.magazov
+ */
+
+#include "command_parser.h"
+
+#define COMMAND_SYMBOL '>'
+#define CHECKSUM_SUMBOL ';'
+
+/* private function prototypes */
+uint8_t isCommandStart(char character);
+uint8_t isCommandBody(char character);
+uint8_t isChecksum(char character);
+uint8_t storeCharacter(command_parser_t *p_parser, char character);
+
+
+void initParser(command_parser_t *p_parser)
+{
+	p_parser->parser_state = ESTATE_IDLE;
+	p_parser->p_head = p_parser->command;
+}
+
+
+void newCharacter(command_parser_t *p_parser, char character)
+{
+	static enum parser_state_t parser_state = ESTATE_IDLE;
+
+	switch (parser_state)
+	{
+	case ESTATE_IDLE:
+		/* This state can transition only into ESTATE_COMMAND upon receiving
+		 * correct command start symbol. */
+		if(isCommandStart(character) != 0)
+		{
+			storeCharacter(p_parser, character);
+			p_parser->parser_state = ESTATE_COMMAND;
+		}
+		break;
+	case ESTATE_COMMAND:
+		/* This state can transition into either ESTATE_CHECKSUM if checksum
+		 * start symbol is received or into ESTATE_IDLE if CRLF is received.
+		 * The state can also transition into itself during accumulation of
+		 * command body. */
+		if(isCommandBody(character) != 0){
+			if(storeCharacter(p_parser, character) == 0)
+			{
+				/* If was unable to store the character due to full buffer,
+				 * reset parser. This will return state machine to IDLE.*/
+				initParser(p_parser);
+			}
+		}
+		else if(is)
+		break;
+	case ESTATE_CHECKSUM:
+		break;
+
+	}
+}
+
+uint8_t isCommandStart(char character)
+{
+	if(character == COMMAND_SYMBOL){
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+
+uint8_t isCommandBody(char character);
+uint8_t isChecksum(char character);
+uint8_t isNewLine(char character);
+uint8_t storeCharacter(command_parser_t *p_parser, char character)
+{
+	/* memory safety checking due to direct operation with pointer. If pointer
+	 * has grown beyond the size of the array, do no allow pointer operation.*/
+	if (((p_parser->p_head) - (p_parser->command)) > MAX_COMMAND_SIZE)
+	{
+		return 0;
+	}
+	else
+	{
+		*(p_parser->p_head) = character;
+		p_parser->p_head++;
+	}
+	return 1;
+}
+
