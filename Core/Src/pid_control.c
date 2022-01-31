1f307911f6d2fc06483a96555e511e4ff0b99ac7
Niuslar <niuslar@outlook.com>
2022-01-31 11:41:43 +0000

    Apply clang format

--- Core/Src/pid_control.c
+++ Core/Src/pid_control.c
@@ -7,134 +7,137 @@
  *      Author: niuslar
  */
 
-
 #include "pid_control.h"
-#include "math.h"
 #include <float.h>
+#include "math.h"
 
 /* Private defines */
-#define DEFAULT_KP		1
-#define DEFAULT_KI		0
-#define DEFAULT_KD		0
+#define DEFAULT_KP 1
+#define DEFAULT_KI 0
+#define DEFAULT_KD 0
 
 /* local PFP. */
 float checkLimits(float value, float max_limit, float min_limit);
 
-
-void initPID(pid_handle_t* p_pid_handler)
+void initPID(pid_handle_t *p_pid_handler)
 {
-	p_pid_handler->kd = DEFAULT_KD;
-	p_pid_handler->ki = DEFAULT_KI;
-	p_pid_handler->kp = DEFAULT_KP;
-
+    p_pid_handler->kd = DEFAULT_KD;
+    p_pid_handler->ki = DEFAULT_KI;
+    p_pid_handler->kp = DEFAULT_KP;
 
 #ifdef LIMIT_CHECKING
-	p_pid_handler->max_p = FLT_MAX;
-	p_pid_handler->min_p = -FLT_MAX;
-	p_pid_handler->max_i = FLT_MAX;
-	p_pid_handler->min_i = -FLT_MAX;
-	p_pid_handler->max_d = FLT_MAX;
-	p_pid_handler->min_d = -FLT_MAX;;
-	p_pid_handler->max_out = FLT_MAX;
-	p_pid_handler->min_out = -FLT_MAX;
+    p_pid_handler->max_p = FLT_MAX;
+    p_pid_handler->min_p = -FLT_MAX;
+    p_pid_handler->max_i = FLT_MAX;
+    p_pid_handler->min_i = -FLT_MAX;
+    p_pid_handler->max_d = FLT_MAX;
+    p_pid_handler->min_d = -FLT_MAX;
+    ;
+    p_pid_handler->max_out = FLT_MAX;
+    p_pid_handler->min_out = -FLT_MAX;
 #endif
-	resetPID(p_pid_handler);
+    resetPID(p_pid_handler);
 }
 
-
-void resetPID(pid_handle_t* p_pid_handler)
+void resetPID(pid_handle_t *p_pid_handler)
 {
-	p_pid_handler->old_output = 0;
-	p_pid_handler->old_error = 0;
-	p_pid_handler->old_integral = 0;
+    p_pid_handler->old_output = 0;
+    p_pid_handler->old_error = 0;
+    p_pid_handler->old_integral = 0;
 }
 
-
-float runPID(pid_handle_t* p_pid_handler, float target_value, float actual_value)
+float runPID(pid_handle_t *p_pid_handler,
+             float target_value,
+             float actual_value)
 {
-	if(p_pid_handler == NULL)
-	{
-		return 0;
-	}
+    if (p_pid_handler == NULL)
+    {
+        return 0;
+    }
 
-	float output = p_pid_handler->old_integral;
-	float pid_value;
-	float error = target_value - actual_value;
+    float output = p_pid_handler->old_integral;
+    float pid_value;
+    float error = target_value - actual_value;
 
-	/* proportional coefficient */
-	if(p_pid_handler->kp != 0)
-	{
-		pid_value = error * p_pid_handler->kp;
+    /* proportional coefficient */
+    if (p_pid_handler->kp != 0)
+    {
+        pid_value = error * p_pid_handler->kp;
 #ifdef LIMIT_CHECKING
-		pid_value = checkLimits(pid_value, p_pid_handler->max_p, p_pid_handler->min_p);
+        pid_value =
+            checkLimits(pid_value, p_pid_handler->max_p, p_pid_handler->min_p);
 #endif
-		output += pid_value;
-	}
+        output += pid_value;
+    }
 
-	/* differential coefficient */
-	if(p_pid_handler->kd != 0)
-	{
-		/*for now we assume that this loop is called at regular intervals, so
-		 * no timing is necessary. */
-		pid_value = (error - p_pid_handler->old_error) * p_pid_handler->ki;
+    /* differential coefficient */
+    if (p_pid_handler->kd != 0)
+    {
+        /*for now we assume that this loop is called at regular intervals, so
+         * no timing is necessary. */
+        pid_value = (error - p_pid_handler->old_error) * p_pid_handler->ki;
 #ifdef LIMIT_CHECKING
-		pid_value = checkLimits(pid_value, p_pid_handler->max_d, p_pid_handler->min_d);
+        pid_value =
+            checkLimits(pid_value, p_pid_handler->max_d, p_pid_handler->min_d);
 #endif
-		output += pid_value;
-	}
+        output += pid_value;
+    }
 
-	/* integral coefficient. this is done somewhat out of regular sequence to
-	 * facilitate limit checking of the integral accumulator and output. */
-	if(p_pid_handler->ki != 0)
-	{
-		/*for now we assume that this loop is called at regular intervals, so
-		 * no timing is necessary. */
-		pid_value = error * p_pid_handler->ki;
+    /* integral coefficient. this is done somewhat out of regular sequence to
+     * facilitate limit checking of the integral accumulator and output. */
+    if (p_pid_handler->ki != 0)
+    {
+        /*for now we assume that this loop is called at regular intervals, so
+         * no timing is necessary. */
+        pid_value = error * p_pid_handler->ki;
 #ifdef LIMIT_CHECKING
-		pid_value = checkLimits(pid_value, p_pid_handler->max_i, p_pid_handler->min_i);
+        pid_value =
+            checkLimits(pid_value, p_pid_handler->max_i, p_pid_handler->min_i);
 #endif
-		output += pid_value;
+        output += pid_value;
 
 #ifdef LIMIT_CHECKING
-		/* If output is saturated at max or min limits, then integral
-		 * accumulator should be prevented from incrementing/decrementing. */
-		if(output > p_pid_handler->max_out){
-			output = p_pid_handler->max_out;
-			/* since output is saturated at max value, prevent integral from increasing.*/
-			if(pid_value > 0)
-			{
-				pid_value = 0;
-			}
-		}
-		if(output < p_pid_handler->min_out){
-			output = p_pid_handler->min_out;
-			/* since output is saturated at min value, prevent integral from decreasing.*/
-			if(pid_value < 0)
-			{
-				pid_value = 0;
-			}
-		}
+        /* If output is saturated at max or min limits, then integral
+         * accumulator should be prevented from incrementing/decrementing. */
+        if (output > p_pid_handler->max_out)
+        {
+            output = p_pid_handler->max_out;
+            /* since output is saturated at max value, prevent integral from
+             * increasing.*/
+            if (pid_value > 0)
+            {
+                pid_value = 0;
+            }
+        }
+        if (output < p_pid_handler->min_out)
+        {
+            output = p_pid_handler->min_out;
+            /* since output is saturated at min value, prevent integral from
+             * decreasing.*/
+            if (pid_value < 0)
+            {
+                pid_value = 0;
+            }
+        }
 #endif
-	}
-	p_pid_handler->old_error = error;
-	p_pid_handler->old_integral += pid_value;
-	p_pid_handler->old_output = output;
-	return output;
+    }
+    p_pid_handler->old_error = error;
+    p_pid_handler->old_integral += pid_value;
+    p_pid_handler->old_output = output;
+    return output;
 }
-
 
 #ifdef LIMIT_CHECKING
 float checkLimits(float value, float max_limit, float min_limit)
 {
-	if(value > max_limit)
-	{
-		value = max_limit;
-	}
-	if(value < min_limit)
-	{
-		value = min_limit;
-	}
-	return value;
+    if (value > max_limit)
+    {
+        value = max_limit;
+    }
+    if (value < min_limit)
+    {
+        value = min_limit;
+    }
+    return value;
 }
 #endif
-
